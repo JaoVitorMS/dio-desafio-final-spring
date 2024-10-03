875b8684c40067e0c7e366c31ab98e0db7687e9d
João Vitor <joao.vitorms1509@gmail.com>
2024-10-03 05:14:20 -0300

    first commit

---
+++ src/test/java/org/zsh/biblioteca_dio/BibliotecaDioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zsh.biblioteca_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
