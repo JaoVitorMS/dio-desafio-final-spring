a277b188dc5e460d5904e6737e396065e17f0b2d
João Vitor <joao.vitorms1509@gmail.com>
2024-10-03 05:44:41 -0300

    finish feature/criando-model-livro

--- src/main/java/org/zsh/biblioteca_dio/domain/Livro.java
+++ src/main/java/org/zsh/biblioteca_dio/domain/Livro.java
@@ -1,4 +1,28 @@
 package org.zsh.biblioteca_dio.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Livros")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Livro {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
 }
